9091aeb34c40577eed2f2a337e23b2f1e2d786f5
Bernard Lefebvre <blefebvre@aptitudetech.net>
2018-05-30 02:58:01 +0000

    Minor fix

--- electronic_funds_transfer/electronic_funds_transfer/report/eft_for_email/eft_for_email.py
+++ electronic_funds_transfer/electronic_funds_transfer/report/eft_for_email/eft_for_email.py
@@ -6,32 +6,33 @@ import frappe
 from frappe import _
 
 def execute(filters=None):
-	columns, data = [], []
-	columns = [_("Invoice") + "::120", _("Date") + "::140",
-                _("Total") + "::120", _("Payment Amount") + "::140", _("Credit") + "::120"]
-	conditions = ""
-	if filters.get("supplier"):
+        columns, data = [], []
+        columns = [_("Invoice") + "::120", _("Date") + "::140",
+                _("Amount") + "::120", _("Credit") + "::120", _("Net Amount") + "::140"]
+        conditions = ""
+        if filters.get("supplier"):
                 conditions += " and efti.supplier = %(supplier)s"
 
-	if filters.get("eft"):
-		conditions += " and eft.name = %(eft)s"
+        if filters.get("eft"):
+                conditions += " and eft.name = %(eft)s"
 
-	data = get_data(conditions, filters)
-	nb_row = len(data)
-	x = 0
-	total_payment_amount = total_invoice = 0.00
+        data = get_data(conditions, filters)
+        nb_row = len(data)
+        x = 0
+        total_payment_amount = total_invoice = total_credit = 0.00
 
-	while x < nb_row:
-		total_invoice = float(data[x][2]) + float(total_invoice)
-		total_payment_amount = float(data[x][2]) + float(total_payment_amount)
-		x += 1
-	
-	data.insert(int(x),["", "Total", round(total_invoice, 2), round(total_payment_amount, 2)])
+        while x < nb_row:
+                total_invoice = float(data[x][2]) + float(total_invoice)
+                total_credit = float(data[x][3]) + float(total_credit)
+                total_payment_amount = float(data[x][4]) + float(total_payment_amount)
+                x += 1
 
-	return columns, data
+        data.insert(int(x),["", "Total", round(total_invoice, 2), round(total_credit, 2), round(total_payment_amount, 2)])
+
+        return columns, data
 
 def get_data(conditions, filters):
-        time_sheet = frappe.db.sql(""" SELECT efti.bill_no, p.bill_date, p.grand_total, efti.grand_total, efti.credit
+        time_sheet = frappe.db.sql(""" SELECT efti.bill_no, p.bill_date, p.grand_total, efti.credit, efti.grand_total
                 FROM `tabAdvance Automatic Payment Tool` eft
                 LEFT JOIN `tabAdvance Automatic Payment Tool Item` efti ON  eft.name = efti.parent
                 LEFT JOIN `tabPurchase Invoice` p ON p.name = efti.purchase_invoice
